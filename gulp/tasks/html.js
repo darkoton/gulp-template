2e92d950ebeb2cb0e2a8e3a45b7e6fa35244cb53
Your Name <you@example.com>
2024-06-09 21:55:13 +0300

    add fls template

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,14 +1,30 @@
 import fileinclude from "gulp-file-include";
 import webHtmlNosvg from "gulp-webp-html-nosvg";
+import versionNumber from "gulp-version-number";
 
 export const html = () => {
-  return app.gulp.src(`${app.path.srcFolder}/html/*.html`, { soursemaps: true })
+  return app.gulp.src(app.path.src.html)
     .pipe(app.plugins.plumber(app.plugins.notify.onError({
       title: "HTML",
       message: "Error: <%= error.message %>"
     })))
     .pipe(fileinclude())
-    .pipe(webHtmlNosvg())
-    .pipe(app.gulp.dest(`${app.path.buildFolder}/`))
-    .pipe(app.plugins.browsersync.stream())
+    .pipe(app.plugins.replace(/@img\//g, 'img/'))
+    .pipe(app.plugins.if(app.isBuild, webHtmlNosvg()))
+    .pipe(app.plugins.if(app.isBuild, versionNumber({
+      "value": "%DT%",
+      "append": {
+        "key": "_v",
+        "cover": 0,
+        "to": [
+          "css",
+          "js"
+        ]
+      },
+      "output": {
+        "file": "gulp/version.json"
+      }
+    })))
+    .pipe(app.gulp.dest(app.path.build.html))
+    .pipe(app.plugins.browsersync.stream());
 }
