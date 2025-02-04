3ac593e3caa43fa58694064995f17f322804b113
darkoto12 <darkoto12@gmail.com>
2025-02-04 10:49:16 +0200

    update js task

--- gulp/tasks/js.js
+++ gulp/tasks/js.js
@@ -1,19 +1,8 @@
 import webpack from "webpack-stream"
 
 export const js = () => {
-  return app.gulp.src(`${app.path.srcFolder}/js/script.js`, { soursemaps: true })
-    .pipe(app.plugins.plumber(app.plugins.notify.onError({
-      title: "JS",
-      message: "Error: <%= error.message %>"
-    })))
-    .pipe(webpack({
-      mode: "development",
-      output: {
-        filename: "script.js"
-      }
-    }))
-    .pipe(app.gulp.dest(`${app.path.buildFolder}/js/`))
-    .pipe(app.gulp.src([`${app.path.srcFolder}/js/*.js`, `!${app.path.srcFolder}/js/script.js`]))
-    .pipe(app.gulp.dest(`${app.path.buildFolder}/js/`))
-    .pipe(app.plugins.browsersync.stream())
+  return app.gulp
+  .src(`${app.path.srcFolder}/js/**/*.js`)
+  .pipe(app.gulp.dest(`${app.path.buildFolder}/js`))
+  .pipe(app.plugins.browsersync.stream());
 }
