e24025337bb592b2caeee8329324fe727b8891dd
darkoto12 <darkoto12@gmail.com>
2026-06-17 19:04:38 +0300

    refoctor: delete unuse postcss config, change format for tailwind style file, delte comment code in config file

--- gulp/configs/config.js
+++ gulp/configs/config.js
@@ -1,8 +1,3 @@
-import tailwindModule from './packages/tailwind/tasks/module.js';
-import {
-  tailwindHTML_CDN,
-  tailwindJS_CDN,
-} from './packages/tailwind/tasks/cdn.js';
 
 import { projectConfig } from '../../project.config.js';
 
@@ -13,31 +8,3 @@ const validateConfig = cfg => {
 };
 
 export const config = validateConfig(projectConfig);
-
-// export default {
-//   packages: {
-//     tailwind: {
-//       enable: false,
-//       config: {
-//         type: 'module', // 'cdn' || 'module'
-//       },
-//       tasks: {
-//         cdn: {
-//           html: tailwindHTML_CDN,
-//           style: null,
-//           js: tailwindJS_CDN,
-//         },
-//         module: {
-//           html: null,
-//           style: tailwindModule,
-//           js: null,
-//         },
-//       },
-//     },
-//   },
-//   gulp: {
-//     images: {
-//       mode: 'all', // 'all' || 'webp' || 'avif'  - Image optimization configuration
-//     },
-//   },
-// };
