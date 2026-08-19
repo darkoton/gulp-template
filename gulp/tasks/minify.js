2b04b09b2670307240c35a5d7e16e9dc05832a9f
darkoto12 <darkoto12@gmail.com>
2026-08-19 19:53:16 +0300

    feat: enhance minification tasks with conditional gulpIf and update favicon links for better structure

--- gulp/tasks/minify.js
+++ gulp/tasks/minify.js
@@ -2,31 +2,37 @@ import htmlmin from 'gulp-htmlmin';
 import cssmin from 'gulp-cssmin';
 import jsmin from 'gulp-jsmin';
 import { optimizeImage } from '../plugins/sharp.js';
+import gulpIf from 'gulp-if';
 
 export const minHTML = () => {
   return app.gulp
     .src(app.paths.globs.htmlBuild)
-    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(
+      gulpIf(
+        app.config.optimization.minify.html,
+        htmlmin({ collapseWhitespace: true }),
+      ),
+    )
     .pipe(app.gulp.dest(app.paths.build));
 };
 
 export const minCSS = () => {
   return app.gulp
     .src(app.paths.globs.stylesBuild)
-    .pipe(cssmin())
+    .pipe(gulpIf(app.config.optimization.minify.css, cssmin()))
     .pipe(app.gulp.dest(app.paths.buildStyles));
 };
 
 export const minJS = () => {
   return app.gulp
     .src(app.paths.globs.scriptsBuild)
-    .pipe(jsmin())
+    .pipe(gulpIf(app.config.optimization.minify.js, jsmin()))
     .pipe(app.gulp.dest(app.paths.buildScripts));
 };
 
 export const minImg = async () => {
   return app.gulp
     .src(app.paths.globs.imagesBuild)
-    .pipe(optimizeImage())
+    .pipe(gulpIf(app.config.optimization.minify.images, optimizeImage()))
     .pipe(app.gulp.dest(app.paths.buildImages));
 };
